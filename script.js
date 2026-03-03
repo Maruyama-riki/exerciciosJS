5f8184bec62ce0f6f6ba96bdf24a17e3f60759e8
Maruyama-riki <gabrielriki1214@gmail.com>
2026-03-03 21:12:49 +0000

    2

---
+++ script.js
@@ -0,0 +1,9 @@
+function multiplicarValorPorDois(numeros) {
+    return numero * 2;
+
+}
+
+let numero =4;
+let resultado = multiplicarValorPorDois(numero);
+let resultado2 = multiplicarValorPorDois(5)
+           
